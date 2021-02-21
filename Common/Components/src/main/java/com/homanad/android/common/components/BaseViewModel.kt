7b871228b9d738e55f3b863d120010fd92b473b9
hmman_dev <hmman.ctu@gmail.com>
2021-02-21 16:02:56 +0700

    Add BaseViewModel

---
+++ Common/Components/src/main/java/com/homanad/android/common/components/BaseViewModel.kt
@@ -0,0 +1,14 @@
+package com.homanad.android.common.components
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+open class BaseViewModel : ViewModel() {
+
+    protected val disposables = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
+}
