f3a7da1b2d24b3adce5f1691f04425a4b2899e6b
hmman_dev <hmman.ctu@gmail.com>
2021-02-21 16:29:15 +0700

    Add DiffCallback

---
+++ Common/Components/src/main/java/com/homanad/android/common/components/DiffCallback.kt
@@ -0,0 +1,18 @@
+package com.homanad.android.common.components
+
+import androidx.recyclerview.widget.DiffUtil
+
+class DiffCallback<T>(
+    private val oldList: List<T>, private val newList: List<T>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition] == newList[newItemPosition]
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition] == newList[newItemPosition]
+}
